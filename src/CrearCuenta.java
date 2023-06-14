8eb7f441bc1b7429a0e501b48eff982fad017a28
Felix <felixnicolas-m@hotmail.com>
2023-06-14 17:35:06 -0300

    CrearCuenta - POO - Objeto: primeraCuenta

---
+++ src/CrearCuenta.java
@@ -0,0 +1,9 @@
+public class CrearCuenta {
+    public static void main(String[] args) {
+
+        Cuenta primeraCuenta = new Cuenta(); // la clase opera como tipo de dato al declarar el objeto
+        primeraCuenta.saldo = 1000;
+
+        System.out.println(primeraCuenta.saldo);
+    }
+}
