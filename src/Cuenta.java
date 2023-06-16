311e37f53f459b3fb81ea5475af5dc1deeb98a01
Felix <felixnicolas-m@hotmail.com>
2023-06-15 22:38:57 -0300

    PruebaMetodos - POO - Referenciación de objetos

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -5,7 +5,28 @@ public class Cuenta {
     String titular;
 
     void depositar(double valor) {
-        saldo = saldo + valor;
+        this.saldo += valor;
+    }
+
+    public boolean retirar(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean trasferir(double valor, Cuenta cuenta) {
+        if (this.saldo>=valor){
+            this.saldo = this.saldo - valor;
+            //cuenta.saldo = cuenta.saldo + valor;
+            cuenta.depositar(valor);
+            return true;
+        }else{
+            return false;
+        }
+
 
     }
 }
